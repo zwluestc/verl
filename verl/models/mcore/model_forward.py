[megatron] fix: Adjust the attention mask shape for VLM with Megatron on NPU (#5945)
### What does this PR do?

> Add **concise** overview of what this PR aims to achieve or
accomplish. Reference related GitHub issues and PRs that help with the
review.

detail in this issue: #5878  

In short, the VLM+Megatron pipeline has seen little usage on NPUs, so it
was not previously adapted to the NPU environment. This PR made the
adapted for it.

### Checklist Before Starting

- [x] Search for similar PRs. Paste at least one query link here: ...
- [x] Format the PR title as `[{modules}] {type}: {description}` (This
will be checked by the CI)
- `{modules}` include `fsdp`, `megatron`, `veomni`, `sglang`, `vllm`,
`rollout`, `trainer`, `ci`, `training_utils`, `recipe`, `hardware`,
`deployment`, `ray`, `worker`, `single_controller`, `misc`, `perf`,
`model`, `algo`, `env`, `tool`, `ckpt`, `doc`, `data`, `cfg`, `reward`,
`fully_async`, `one_step_off`
- If this PR involves multiple modules, separate them with `,` like
`[megatron, fsdp, doc]`
  - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
- If this PR breaks any API (CLI arguments, config, function signature,
etc.), add `[BREAKING]` to the beginning of the title.
  - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`

### Test

> For changes that can not be tested by CI (e.g., algorithm
implementation, new model support), validate by experiment(s) and show
results like training curve plots, evaluation results, etc.

### API and Usage Example

> Demonstrate how the API changes if any, and provide usage example(s)
if possible.

```python
# Add code snippet or script demonstrating how to use this
```

### Design & Code Changes

> Demonstrate the high-level design if this PR is complex, and list the
specific changes.

### Checklist Before Submitting

> [!IMPORTANT]
> Please check all the following items before requesting a review,
otherwise the reviewer might deprioritize this PR for review.

- [x] Read the [Con

verl/models/mcore/model_forward.py
@@ -13,17 +13,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import math
 from typing import Optional
 
 import torch
-from megatron.core import parallel_state as mpu
 from torch.nested._internal.nested_tensor import NestedTensor
 
 from verl.utils.megatron_utils import unwrap_model
 from verl.workers.config import MtpConfig
 
 from .util import (
+    build_vlm_attn_mask_bshd,
+    build_vlm_attn_mask_thd,
     postprocess_bshd,
     postprocess_bshd_engine,
     postprocess_packed_seqs,
@@ -279,11 +279,7 @@ def gptmodel_forward_model_engine(
         # For VLM model, need to pass bshd format `input_ids` and `attention_mask`.
         attention_mask = None
         if vision_model:
-            input_ids_rmpad = input_ids.to_padded_tensor(pad_token_id)
-            seqlens_in_batch = input_ids.offsets().diff()
-            attention_mask = torch.zeros_like(input_ids_rmpad, dtype=torch.bool)
-            for i, seqlen in enumerate(seqlens_in_batch):
-                attention_mask[i, :seqlen] = True
+            input_ids_rmpad, attention_mask = build_vlm_attn_mask_thd(input_ids, pad_token_id)
 
         output_orig = model(
             input_ids=input_ids_rmpad,
@@ -354,23 +350,10 @@ def gptmodel_forward_model_engine(
             logits_processor_args.pop("loss_mask")
 
         # For VLM model, need to pass bshd format `input_ids` and `attention_mask`.
-        attention_mask = attention_mask_bshd
         if vision_model:
-            seqlens_in_batch = input_ids.offsets().diff()
-            max_seqlen = seqlens_in_batch.max().item()
-
-            # For CP, sequence length must be divisible by (2 * cp_size), and for SP by tp_size.
-            tp_size = mpu.get_tensor_model_parallel_world_size()
-            cp_size = mpu.get_context_parallel_world_size()
-            align_size = math.lcm(tp_size, 2 * cp_size) if cp_size > 1 else tp_size
-            if align_size > 1:
-                pad_size = (align_size - max_seqlen % align_size) % align_size
-                max_seqlen += pad_size
-
-            input_ids_bshd = input_ids.to_padded_tensor(pad_token_id, output_size=(batch_size, max_seqlen))
-            attention_mask = torch.zeros_like(input_ids_bshd, dtype=torch.bool)
-            for i, seqlen in enumerate(seqlens_in_batch):
-                attention_mask[i, :seqlen] = True
+            input_ids_bshd, attention_mask = build_vlm_attn_mask_bshd(input_ids, batch_size, pad_token_id)
+        else:
+            attention_mask = attention_mask_bshd
 
         output_orig = model(
             input_ids=input_ids_bshd,